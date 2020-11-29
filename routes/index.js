040bce8b669d9d78c4775b9c6a6a61de68136ebf
superoft1 <superoft1@users.noreply.github.com>
2020-11-29 11:07:33 +0700

    update home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {layout: 'mainlayout'});
+  res.render('index', {layout: 'main_layout'});
 });
 
 module.exports = router;
