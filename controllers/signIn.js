c7a6ef85ccdd7c5d13dd39381fa8cf324f204be8
daiphu0605 <57994005+daiphu0605@users.noreply.github.com>
2020-11-30 15:18:36 +0700

    update signIn and signUp in routes and controllers

---
+++ controllers/signIn.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+exports.SignIn = (req, res) => {
+    var username = req.body.usernam;
+    var pass = req.body.pass;
+    var Error = "";
+
+    return true;
+}
