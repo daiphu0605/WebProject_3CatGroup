95df565138d446d5764db186154c90cb6bb14879
daiphu0605 <57994005+daiphu0605@users.noreply.github.com>
2020-11-30 20:58:03 +0700

    update routes: signin và signup; update sign view

--- routes/signup.js
+++ routes/signup.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var signUp = require('../controllers/signUp')
 
 router.get('/', (req, res, next) => {
     res.render('sign_up', {layout: 'layout_sign'});
 });
 
 router.post('/', (req, res) => {
-    var SignUp = {
-        username: req.body.username,
-        password: req.body.pass,
-    };
-    res.render('sign_up_fin', {layout: 'layout_sign'});
+    signUp.SignUp(req,res);
 });
 
 router.get('/', (req, res, next) => {
