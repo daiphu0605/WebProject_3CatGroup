43b4dabecb872e213297b651fa0024ae38064145
daiphu0605 <57994005+daiphu0605@users.noreply.github.com>
2021-01-13 20:36:18 +0700

    update forget

--- controllers/forgetPassword.js
+++ controllers/forgetPassword.js
@@ -24,7 +24,7 @@ exports.forgetHandler = (req, res, next) => {
 
 function randString(length) {
    var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=-*';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
    var charactersLength = characters.length;
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
